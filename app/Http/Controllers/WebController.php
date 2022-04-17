83e791ed3f73c05e28d851c292e426e1e5c8eb45
fahmihwan <fahmiiwan86@gmail.com>
2022-04-17 15:16:41 +0700

    first

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function payment(Request $request)
+    {
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = 'SB-Mid-server-l4ja6huIEGPSjPi5oGsTKesl';
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
+
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => rand(),
+                'gross_amount' => 1000,
+            ),
+            'item_details' => array(
+                [
+                    "id" => "a01",
+                    "price" => 500,
+                    "quantity" => 1,
+                    "name" => "Apple"
+                ],
+                [
+                    "id" => "a01",
+                    "price" => 500,
+                    "quantity" => 1,
+                    "name" => "Apple"
+                ],
+
+            ),
+            'customer_details' => array(
+                'first_name' => $request->nama,
+                'last_name' => '',
+                'email' => $request->email,
+                'phone' => $request->telp,
+            ),
+        );
+
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+        return view('payment', [
+            'snap_token' => $snapToken
+        ]);
+    }
+
+    public function payment_post(Request $request)
+    {
+        $json = json_decode($request->json);
+
+        $orderModel = new Order();
+        $orderModel->status = $json->transaction_status;
+        $orderModel->name = $request->name;
+        $orderModel->email = $request->email;
+        $orderModel->number = $request->number;
+        $orderModel->transaction_id = $json->transaction_id;
+        $orderModel->order_id = $json->order_id;
+        $orderModel->gross_amount = $json->gross_amount;
+        $orderModel->payment_type = $json->payment_type;
+        $orderModel->payment_code = isset($json->payment_code) ? $json->payment_code : null;
+        $orderModel->pdf_url = isset($json->pdf_url) ? $json->pdf_url : null;
+
+        // dd($orderModel);
+        $orderModel->save();
+        return redirect('/')->with('success', $json->transaction_status);
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body class="bg-dark">
+    @if (session()->has('success'))
+        <p class="text-light"> {{ session('success') }}</p>
+    @endif
+
+    <div class="container">
+        <div class="row justify-content-center align-items-center" style="height: 100vh;">
+            <form action="/payment" class="card rounded p-5 bg-secondary" style="width: 300px">
+                @csrf
+                <div class="mb-3">
+                    <label for="nama" class="form-label">Nama</label>
+                    <input type="text" class="form-control" id="name" placeholder="nama" name="name">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" placeholder="name@example.com" name="email">
+                </div>
+                <div class="mb-3">
+                    <label for="pw" class="form-label">Telp</label>
+                    <input type="telp" class="form-control" id="pw" placeholder="telp" name="number">
+                </div>
+                <button class="btn btn-warning mt-4">login</button>
+            </form>
+
+        </div>
+
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+    -->
+</body>
+
+</html>
